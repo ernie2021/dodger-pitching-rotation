9cd819563b3734214493f15cde5e00a5e2072e0c
Ernesto Garcia <egarcia2020@yahoo.com>
2022-04-17 20:51:50 -0700

    added code and changes

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+document.getElementsByTagName("button").onclick = function () {
+    location.href = "https://www.mlb.com/dodgers/video/trea-turner-s-two-run-dinger?t=t119-default-vtp";
+
+};
+
+
+// function DodgerBlue() { 
+//     if (dodgerBlue.onclick()) {
+//         location.href = "https://www.mlb.com/dodgers/video/trea-turner-s-two-run-dinger?t=most-valuable-performances";
+//     } else {
+//         location.href = "www.dodgers.com"
+//     }
+// }
+
+// DodgerBlue(); 
